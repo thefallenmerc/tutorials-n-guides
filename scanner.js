c2ddf27ff58ea37857cfbe98318194d5775ce7e7
Shubham Singh Chahar <thefallenmerc@gmail.com>
2019-12-26 23:23:06 +0530

    initial commit

---
+++ scanner.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
+app.use(express.static('static')).listen(2323);
+
+const staticDir = 'static';
+
+const source = path.join(__dirname, staticDir, 'src');
+
+console.log('Starting the scan...');
+fs.writeFileSync(staticDir + '/content.json', JSON.stringify(scanDirectoryRecursive(source), null, 2));
+console.log("Scan successful!");
+
+function scanDirectoryRecursive(route = './') {
+
+    const structure = [];
+
+    for (let item of fs.readdirSync(route)) {
+        const itemPath = path.join(route, item);
+        const itemStat = fs.lstatSync(itemPath);
+        if(itemStat.isDirectory()) {
+            structure.push({
+                content: scanDirectoryRecursive(itemPath),
+                path: itemPath.replace(path.join(__dirname, staticDir), '').replace(/\\/g, '/', ),
+                name: item,
+                type: 'directory'
+            });
+        } else {
+            structure.push({
+                name: item,
+                path: itemPath.replace(path.join(__dirname, staticDir), '').replace(/\\/g, '/', ),
+                type: 'file'
+            });
+        }
+    }
+    return structure;
+}
