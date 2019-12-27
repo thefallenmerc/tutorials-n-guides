2277632fa1f84d799e4b21d09adc3d413982caa9
Shubham Singh Chahar <thefallenmerc@gmail.com>
2019-12-27 09:37:53 +0530

    automatic updated push

--- scanner.js
+++ scanner.js
@@ -6,14 +6,7 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin', ['*']);
-    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.append('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
-
-app.use(express.static('static')).listen(2323);
+const port = 2323;
 
 const staticDir = 'static';
 
@@ -23,6 +16,21 @@ console.log('Starting the scan...');
 fs.writeFileSync(staticDir + '/content.json', JSON.stringify(scanDirectoryRecursive(source), null, 2));
 console.log("Scan successful!");
 
+if (!process.argv.includes('--scanonly')) {
+    app.use((req, res, next) => {
+        res.append('Access-Control-Allow-Origin', ['*']);
+        res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.append('Access-Control-Allow-Headers', 'Content-Type');
+        next();
+    });
+
+    app.use(express.static('static')).listen(port);
+
+    console.log('Listening at the port: ' + port);
+    console.log('Server at: http://localhost:' + port);
+}
+
+
 function scanDirectoryRecursive(route = './') {
 
     const structure = [];
@@ -30,17 +38,17 @@ function scanDirectoryRecursive(route = './') {
     for (let item of fs.readdirSync(route)) {
         const itemPath = path.join(route, item);
         const itemStat = fs.lstatSync(itemPath);
-        if(itemStat.isDirectory()) {
+        if (itemStat.isDirectory()) {
             structure.push({
                 content: scanDirectoryRecursive(itemPath),
-                path: itemPath.replace(path.join(__dirname, staticDir), '').replace(/\\/g, '/', ),
+                path: itemPath.replace(path.join(__dirname, staticDir), '').replace(/\\/g, '/'),
                 name: item,
                 type: 'directory'
             });
         } else {
             structure.push({
                 name: item,
-                path: itemPath.replace(path.join(__dirname, staticDir), '').replace(/\\/g, '/', ),
+                path: itemPath.replace(path.join(__dirname, staticDir), '').replace(/\\/g, '/'),
                 type: 'file'
             });
         }
